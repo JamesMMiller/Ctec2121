2dcc1361ba4cbbd4c2b9858b5d8dd4b373176776
James <J.malachy.miller@gmail.com>
2018-01-23 12:24:03 +0000

    Hello world c program commited

--- hello.c
+++ hello.c
@@ -1,8 +1,5 @@
-#include <stdio.h>
-int main()
-{
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+#include<stdio.h> 
+main()
+{    
+   printf("Hello World\n");
 }
-
